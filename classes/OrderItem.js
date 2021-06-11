fb547b388001bbcdc8832e46c24a964d7a262815
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-06-11 15:26:47 -0400

    added default value

--- classes/OrderItem.js
+++ classes/OrderItem.js
@@ -3,7 +3,15 @@ const Order = require("./Order");
 module.exports = class OrderItem {
   constructor() {
     this.state = "";
-    this.name = "";
+    this.name = "medium";
     this.description = "";
+    this.basePrice = 0;
+    this.finalPrice = 0;
+  }
+
+  getTotal() {
+    let total = this.basePrice;
+    this.finalPrice = total;
+    return this.finalPrice;
   }
 };
