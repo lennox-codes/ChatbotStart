255f555f5f225b12a7f51e46b8362900b721aca5
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-06-11 15:26:08 -0400

    added getTotal method

--- classes/Order.js
+++ classes/Order.js
@@ -4,6 +4,7 @@ class Order {
     this.custId = "";
     this.items = [];
     this.states = ["welcoming", "selecting", "isFinished", "moreItems"];
+    this.total = 0;
 
     if (Order._instance) {
       return Order._instance;
@@ -15,31 +16,44 @@ class Order {
     this.state = "welcoming";
     this.items = [];
     this.custId = "";
+    this.total = 0;
   }
 
   addItem(item) {
     this.items.push(item);
   }
 
-  getSummary() {
-    let date = new Date();
-    date.setMinutes(date.getMinutes() + 20);
-    const timeToPickUp = `Please pick up at \n${date.toTimeString()}`;
-
-    let itemSummary = ["You orders are: "];
-    this.items.forEach((item, index) => {
-      itemSummary.push(`${index + 1}. ${item.description}`);
-    });
-
-    itemSummary.push(timeToPickUp);
-
-    this.reset();
-    return itemSummary;
+  formatPrice(price) {
+    return "$" + price.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
   }
 
-  async advance() {
-    const data = await fetch("/sms");
-    //const response =
+  getSummary() {
+    this.getTotal();
+    let date = new Date();
+    date.setMinutes(date.getMinutes() + 20);
+    const timeToPickUp = `Your Please pick up at \n${date.toTimeString()}`;
+
+    let orderSummary = ["You orders are: "];
+    this.items.forEach((item, index) => {
+      orderSummary.push(
+        `${index + 1}. ${item.description} \nPrice: ${this.formatPrice(
+          item.finalPrice
+        )}`
+      );
+    });
+
+    orderSummary.push(
+      `The estimated price of your order is ${this.formatPrice(this.total)} `
+    );
+    orderSummary.push(timeToPickUp);
+
+    this.reset();
+    return orderSummary;
+  }
+
+  getTotal() {
+    this.items.forEach((item) => (this.total += item.finalPrice));
+    return this.total;
   }
 }
 
