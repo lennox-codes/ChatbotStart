13dc9e48a314268afec92d9d6844cb7acdae6383
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-06-11 13:56:57 -0400

    edited properties

--- classes/OrderItem.js
+++ classes/OrderItem.js
@@ -3,9 +3,6 @@ const Order = require("./Order");
 module.exports = class OrderItem {
   constructor() {
     this.state = "";
-    this.size = "";
-    this.toppings = "";
-    this.drinks = "";
     this.name = "";
     this.description = "";
   }
