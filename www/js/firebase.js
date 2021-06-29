fc103de3139dd0f78a413678306c819175d0a490
Lennox Gilbert <lennoxgilbert@yahoo.com>
2021-06-29 13:15:07 -0400

    stopped tracking firebase.js

--- www/js/firebase.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyCUWxZEkCOKXo_5hg4PRu6gj8110a3n7Z0",
-  authDomain: "fir-todo-b8a67.firebaseapp.com",
-  projectId: "fir-todo-b8a67",
-  storageBucket: "fir-todo-b8a67.appspot.com",
-  messagingSenderId: "374449605223",
-  appId: "1:374449605223:web:dc5c4bddb78f9c8bb422c2",
-  measurementId: "G-RGYQP6G8DV",
-};
